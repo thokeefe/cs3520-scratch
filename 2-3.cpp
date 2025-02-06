74f48d34740e9ebcd3d7604b5b7c328ede86f96f
thokeefe <okeefe.th@northeastern.edu>
2025-02-06 13:06:52 -0500

    2-3

--- 2-3.cpp
+++ 2-3.cpp
@@ -8,6 +8,11 @@ You can paste the code into scratch.cpp from the demo to run in VS Code.
 /* ANSWER:
 mystery 1 - 1,141,899,961 (16.37%)
 mystery 2 - 5,831,727,541 (83.61%)
+
+Ir                      file:function
+--------------------------------------------------------------------------------
+5,831,727,541 (83.61%)  2-3.cpp:mystery2()'2 [/home/ubuntu/cs3520-scratch/mystery_program]
+1,141,899,961 (16.37%)  2-3.cpp:mystery1()'2 [/home/ubuntu/cs3520-scratch/mystery_program]
 */
 
 #include <iostream>
