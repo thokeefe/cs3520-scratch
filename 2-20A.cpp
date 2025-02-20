be519861ddd3155a68d73d083a0365736cc3bd4b
thokeefe <okeefe.th@northeastern.edu>
2025-02-20 12:58:58 -0500

    220

---
+++ 2-20A.cpp
@@ -0,0 +1,28 @@
+/*
+Write a function element_of that takes a vector of strings and a string to search for as arguments. The function returns the 
+index of that string in the vector, if it is there, otherwise it throws an runtime_error exception. (Don’t worry about catching 
+the exception for this exercise.)
+*/
+
+#include <iostream>
+#include <stdexcept>
+#include <string>
+#include <vector>
+using namespace std;
+
+size_t index_of(const vector<string> & v, const string & s) {
+  // TODO
+  for (int i; i < v.size(); i++) {
+    if (v[i] == s) {
+        return i;
+    }
+  }
+}
+
+int main() {
+  vector<string> vec = { "a", "b", "c", "d", "e", "f" };
+
+  cout << index_of(vec, "a") << endl; // 0
+  cout << index_of(vec, "d") << endl; // 3
+  cout << index_of(vec, "g") << endl; // exception!
+}
